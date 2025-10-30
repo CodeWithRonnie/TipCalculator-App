e0ad6ad4662a0f4301097740f34af520b8985bac
mamikie maemu <maemu.ronewa98@gmail.com>
2025-10-30 12:48:35 +0200

    created a tip calculator app from scratch using HTML, CSS and JavaScript. We get a bill amount, divide the number of people and th etip they suppose to pay the geth the total of each person.

---
+++ tip-calculator-app-main/script.js
@@ -0,0 +1,17 @@
+
+let billInput = document.querySelector(".input");
+let tip = document.querySelector(".tipAmount");
+let numberOfPeopleInput = document.querySelector(".number-of-people");
+let total = document.querySelector(".total");
+
+function fivePercent() {
+    let bill = parseFloat(billInput.value) || 0;
+    let numberOfPeople = parseFloat(numberOfPeopleInput.value) || 1;
+
+    let tipPerPerson = (bill * 0.05) / numberOfPeople;
+    let totalOfPerson = (bill / numberOfPeople) + tipPerPerson;
+
+    tip.innerHTML = tipPerPerson.toFixed(2);
+    total.innerHTML = totalOfPerson.toFixed(2);
+
+}
